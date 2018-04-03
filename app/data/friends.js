a10cd98240d2d4841bbe08793f51bf0c594fca35
jensweatmon <jenjenhuynh@gmail.com>
2018-04-02 23:12:41 -0400

    initial push

---
+++ app/data/friends.js
@@ -0,0 +1,115 @@
+var friends = [
+
+{
+    name: "Sandra",
+    photo: "https://i.pinimg.com/originals/e5/bf/dc/e5bfdccc7938176c031d446595ea608e.jpg",
+    scores: [
+    4,
+    4,
+    3,
+    2,
+    5,
+    1,
+    4,
+    5,
+    3,
+    4
+    ]
+},
+{
+    name: "George",
+	photo: "http://r.ddmcdn.com/s_f/o_1/TLC/uploads/2014/10/celebrity-psychics-george-clooney.jpg",
+	scores: [
+	3,
+	3,
+	2,
+	5,
+	4,
+	2,
+	1,
+	2,
+	2,
+	4
+	]
+}, {
+
+    name: "Courtney",
+	photo: "https://marriedwiki.com/uploads/bio/courtney-cox.jpg",
+	scores: [
+	2,
+	1,
+	4,
+	3,
+	4,
+	5,
+	5,
+	5,
+	1,
+	1
+	]
+},{
+    name: "Dwayne",
+	photo: "http://www.trbimg.com/img-5ac26b55/turbine/ct-dwayne-the-rock-johnson-depression-20180402",
+	scores: [
+	5,
+	3,
+	1,
+	2,
+	3,
+	1,
+	4,
+	4,
+	1,
+	3
+	]
+}, {
+    name: "Joe",
+	photo: "https://pbs.twimg.com/profile_images/786767617742737408/swbHq0cT_400x400.jpg",
+	scores: [
+	2,
+	1,
+	1,
+	3,
+	5,
+	1,
+	3,
+	2,
+	2,
+	5
+	]
+}, {
+    name: "Jessica",
+	photo: "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_80%2Cw_300/MTE4MDAzNDEwMzY0NDMzOTM0/jessica-alba-299896-1-402.jpg",
+	scores: [
+	3,
+	4,
+	5,
+	5,
+	4,
+	3,
+	2,
+	1,
+	2,
+	1
+	]
+}, {
+
+    name: "Lauren",
+	photo: "https://cdn.cliqueinc.com/posts/244890/lauren-conrad-haircut-244890-1513186196798-main.640x0c.jpg",
+	scores: [
+	3,
+	4,
+	5,
+	5,
+	4,
+	3,
+	2,
+	1,
+	2,
+	1
+	]
+},
+
+]
+
+module.exports = friends;
